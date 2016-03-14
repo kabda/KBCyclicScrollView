6d2d5d64fdd71c2bad65ca4c5c4629e814e68dc9
fanyuandong <fanyuandong@didichuxing.com>
2016-03-14 19:17:38 +0800

    新建

---
+++ Classes/KBCyclicScrollView.swift
@@ -0,0 +1,15 @@
+//
+//  KBCyclicScrollView.swift
+//  KBCyclicScrollView
+//
+//  Created by 樊远东 on 3/14/16.
+//  Copyright © 2016 樊远东. All rights reserved.
+//
+
+import UIKit
+
+class KBCyclicScrollView: UIScrollView {
+
+
+
+}
